1eff4260686fe08a6b7528d500a3f87242fe55a9
Azmol-Miah <azmol@azmolmiah.com>
2019-12-18 13:02:35 +0000

    Letters:
    Fix container margin

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,8 @@
 // Get all the pages to be displayed
-let currentPage = document.querySelector('.letters');
+let currentPage = document.querySelector(".letters");
 
 // Get current number element and add options
-const currentPageNumber = document.getElementById('pageNumber');
+const currentPageNumber = document.getElementById("pageNumber");
 let options;
 for (let i = 0; i <= 40; i++) {
   options += `<option>${i}</option>`;
@@ -10,8 +10,8 @@ for (let i = 0; i <= 40; i++) {
 currentPageNumber.innerHTML += options;
 
 // Get the next or next page element// Get the previous or previous page element
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
 
 // Create new XMLHttpRequest request
 let xhttp = new XMLHttpRequest();
@@ -30,12 +30,12 @@ xhttp.onload = function() {
     let outputThree;
 
     // Add second row
-    const sectionElement = document.createElement('tr');
-    sectionElement.className = 'letters mt-3';
+    const sectionElement = document.createElement("tr");
+    sectionElement.className = "letters mt-3";
     const sectionTwo = currentPage.parentElement.appendChild(sectionElement);
 
-    const sectionElementThree = document.createElement('tr');
-    sectionElementThree.className = 'letters mt-3';
+    const sectionElementThree = document.createElement("tr");
+    sectionElementThree.className = "letters mt-3";
     const sectionThree = currentPage.parentElement.appendChild(
       sectionElementThree
     );
@@ -59,9 +59,9 @@ xhttp.onload = function() {
         while (currentPage.firstChild) {
           currentPage.removeChild(currentPage.firstChild);
         }
-        output = '';
-        this.outputNum = '';
-        this.sectionNum.innerHTML = '';
+        output = "";
+        this.outputNum = "";
+        this.sectionNum.innerHTML = "";
       }
     }
 
@@ -83,11 +83,11 @@ xhttp.onload = function() {
       }
 
       static pageTitle(img, titleHeight) {
-        const newTd = document.createElement('td');
-        newTd.style.padding = '4px';
+        const newTd = document.createElement("td");
+        newTd.style.padding = "4px";
         newTd.style.height = titleHeight;
         newTd.innerHTML = `<img  src="${img}">`;
-        const td = document.getElementsByTagName('td');
+        const td = document.getElementsByTagName("td");
         currentPage.insertBefore(newTd, td[0]);
       }
 
@@ -114,33 +114,31 @@ xhttp.onload = function() {
         } else {
           // if page has one section
           for (let i = 0; i < this.object.length; i++) {
-            output += `<td id="${this.object[i].name}" ><img src="${
-              this.object[i].image
-            }"></td>`;
+            output += `<td id="${this.object[i].name}" ><img src="${this.object[i].image}"></td>`;
             currentPage.innerHTML = output;
           }
         }
-        let letterimg = document.getElementsByTagName('img');
+        let letterimg = document.getElementsByTagName("img");
 
         if (this.object == obj[0]) {
-          Letters.pageTitle('img/firstpagetitle.png', '44.5px');
+          Letters.pageTitle("img/firstpagetitle.png", "44.5px");
           for (let i = 0; i < letterimg.length; i++) {
-            letterimg[0].style.width = '100%';
-            letterimg[0].style.height = '44.5px';
-            letterimg[i].style.width = '80px';
-            letterimg[i].style.height = '70px';
+            letterimg[0].style.width = "100%";
+            letterimg[0].style.height = "44.5px";
+            letterimg[i].style.width = "80px";
+            letterimg[i].style.height = "70px";
           }
         } else if (this.object == obj[1]) {
-          Letters.pageTitle('img/pg2/secondpagetitle.png', '44.5px');
+          Letters.pageTitle("img/pg2/secondpagetitle.png", "44.5px");
           for (let i = 0; i < letterimg.length; i++) {
-            letterimg[0].style.width = '100%';
-            letterimg[i].style.width = '66.3px';
-            letterimg[i].style.height = '44.5px';
+            letterimg[0].style.width = "100%";
+            letterimg[i].style.width = "66.3px";
+            letterimg[i].style.height = "44.5px";
           }
         } else if (this.object == obj[2]) {
           for (let i = 0; i < letterimg.length; i++) {
-            letterimg[i].style.width = '66.3px';
-            letterimg[i].style.height = '44.5px';
+            letterimg[i].style.width = "66.3px";
+            letterimg[i].style.height = "44.5px";
           }
         }
       }
@@ -179,7 +177,7 @@ xhttp.onload = function() {
     } //--Closing brace nextPage()
 
     //Get the previous button and add event
-    prev.addEventListener('click', function() {
+    prev.addEventListener("click", function() {
       if (current === 0) {
         current = obj.length;
       }
@@ -187,7 +185,7 @@ xhttp.onload = function() {
     });
 
     //Get the next button and add event
-    next.addEventListener('click', function() {
+    next.addEventListener("click", function() {
       if (current === obj.length - 1) {
         current = -1;
       }
@@ -196,8 +194,8 @@ xhttp.onload = function() {
 
     // ========================================= SOUND FUNCTION ================================== //
     // Set empty sound variable, Setting the Audio Object, concatenate file extensions
-    const folder = 'audio/';
-    const extension = '.mp3';
+    const folder = "audio/";
+    const extension = ".mp3";
     let sound;
     sound = new Audio();
 
@@ -208,31 +206,31 @@ xhttp.onload = function() {
     // If play loop has reached end of last letter
     function playPauseBtn(objLength, removeAllBgBlue) {
       if (playIndex == objLength) {
-        playPaused.classList.remove('fa-pause');
-        playPaused.classList.add('fa-play');
+        playPaused.classList.remove("fa-pause");
+        playPaused.classList.add("fa-play");
         setTimeout(function() {
           for (let i = 0; i < removeAllBgBlue.length; i++) {
-            removeAllBgBlue[i].classList.remove('bgBlue');
+            removeAllBgBlue[i].classList.remove("bgBlue");
           }
         }, 1000);
       } else {
-        playPaused.classList.remove('fa-play');
-        playPaused.classList.add('fa-pause');
+        playPaused.classList.remove("fa-play");
+        playPaused.classList.add("fa-pause");
       }
     }
 
     let playIndex = 0;
-    let playPaused = document.getElementById('playBtn');
+    let playPaused = document.getElementById("playBtn");
 
-    playPaused.addEventListener('mousedown', () => {
+    playPaused.addEventListener("mousedown", () => {
       if (sound.paused) {
-        playPaused.classList.remove('fa-play');
-        playPaused.classList.add('fa-pause');
+        playPaused.classList.remove("fa-play");
+        playPaused.classList.add("fa-pause");
         switchSound();
       } else {
         sound.pause();
-        playPaused.classList.remove('fa-pause');
-        playPaused.classList.add('fa-play');
+        playPaused.classList.remove("fa-pause");
+        playPaused.classList.add("fa-play");
       }
     });
 
@@ -253,7 +251,7 @@ xhttp.onload = function() {
         sectTwoArr = sectOneArr.concat(
           Array.from(currentPage.parentElement.children[1].children)
         );
-        sectTwoArr[playIndex].classList.add('bgBlue');
+        sectTwoArr[playIndex].classList.add("bgBlue");
         // If Three section
       } else if (obj[current].length == 3) {
         let threeSectionConcat = twoSectionConcat.concat(
@@ -264,15 +262,15 @@ xhttp.onload = function() {
         let sectThreeArr = sectTwoArr.concat(
           Array.from(currentPage.parentElement.children[2].children)
         );
-        sectThreeArr[playIndex].classList.add('bgBlue');
+        sectThreeArr[playIndex].classList.add("bgBlue");
       } else {
         playPauseBtn(obj[current].length, currentPage.children);
         sndSrc(obj[current][playIndex].name);
-        currentPage.children[playIndex].classList.add('bgBlue');
+        currentPage.children[playIndex].classList.add("bgBlue");
       }
     }
 
-    sound.addEventListener('ended', switchSound);
+    sound.addEventListener("ended", switchSound);
     function switchSound() {
       sectionLoop();
       sound.play();
@@ -280,7 +278,7 @@ xhttp.onload = function() {
     }
 
     // Play clicking individual sounds
-    currentPage.parentElement.addEventListener('click', selectSound);
+    currentPage.parentElement.addEventListener("click", selectSound);
     function selectSound(e) {
       let soundTwo = new Audio();
       // Get id name of each sound image thats outputed
@@ -291,7 +289,7 @@ xhttp.onload = function() {
     }
 
     // =========================================== SELECT OPTIONS ======================================= //
-    currentPageNumber.addEventListener('change', options);
+    currentPageNumber.addEventListener("change", options);
 
     function options(e) {
       reset();
@@ -302,25 +300,25 @@ xhttp.onload = function() {
     }
 
     // =========================================== Book Mark ================================== //
-    let pageNumber = JSON.parse(localStorage.getItem('pageNumber'));
+    let pageNumber = JSON.parse(localStorage.getItem("pageNumber"));
     //Local storage set bookmark
-    let bookMarkIcon = document.getElementById('bookmark');
-    bookMarkIcon.addEventListener('mousedown', bookMark);
+    let bookMarkIcon = document.getElementById("bookmark");
+    bookMarkIcon.addEventListener("mousedown", bookMark);
 
     //Check Bookmark and fill if current bookMark
 
     function checkBookMark(pagenumber) {
       if (pageNumber === pagenumber) {
-        bookMarkIcon.classList.remove('fa-bookmark-o');
-        bookMarkIcon.classList.add('fa-bookmark');
+        bookMarkIcon.classList.remove("fa-bookmark-o");
+        bookMarkIcon.classList.add("fa-bookmark");
       } else {
-        bookMarkIcon.classList.remove('fa-bookmark');
-        bookMarkIcon.classList.add('fa-bookmark-o');
+        bookMarkIcon.classList.remove("fa-bookmark");
+        bookMarkIcon.classList.add("fa-bookmark-o");
       }
     }
 
     function bookMark() {
-      localStorage.setItem('pageNumber', JSON.stringify(current));
+      localStorage.setItem("pageNumber", JSON.stringify(current));
       checkBookMark(pageNumber);
       window.location.reload();
     }
@@ -329,13 +327,13 @@ xhttp.onload = function() {
       getBookMark();
     }
 
-    let bookMarkRef = document.getElementById('bookMarkRef');
-    bookMarkRef.addEventListener('mousedown', getBookMark);
+    let bookMarkRef = document.getElementById("bookMarkRef");
+    bookMarkRef.addEventListener("mousedown", getBookMark);
 
     // Local storage get bookmark
     function getBookMark() {
       currentPageNumber.selectedIndex = JSON.parse(
-        localStorage.getItem('pageNumber')
+        localStorage.getItem("pageNumber")
       );
       current = pageNumber;
       reset();
@@ -349,6 +347,6 @@ xhttp.onload = function() {
 }; //--Closing brace on xhttp onreadystate change object
 
 // Use Ajax to get json data locally
-xhttp.open('GET', 'js/qaida.json', true);
+xhttp.open("GET", "js/qaida.json", true);
 //Send ajax request
 xhttp.send();
